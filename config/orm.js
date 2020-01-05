770aa5f0f8230a975017c320bb9bfe19f7831dac
Marissa Fortier <marissafortier@gmail.com>
2020-01-05 15:12:03 -0500

    started creating methods in orm.js and created models folder with burger.js

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,16 @@
-const orm = require("./connection");
+const connection = require("./connection");
+
+var orm = {
+    selectAll : function(whatToSelect, tableInput) {
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [whatToSelect, tableInput], function(err, req) {
+            if(err) throw err;
+            console.log(result);
+        });
+    },
+
+    // insertOne: function(tableInput) {
+    //     var queryString =
+    // }
+}
 
