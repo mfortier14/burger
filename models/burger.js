389a7f81c97339feb7c696803377f606db6c416f
Marissa Fortier <marissafortier@gmail.com>
2020-01-05 16:35:03 -0500

    added controllers file and created burgers_controllers.js file

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,9 @@
 const ORM = require("../config/orm");
 
+// module.exports = function(sequelize, DataTypes) {
+//     var Order = sequelize.define("Post", {
+//         burger: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         }
+//     },
